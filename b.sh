e009a2b5828ee0aab558303c6433a62cc618aefb
Ubuntu <ubuntu@ip-172-31-26-162.us-west-2.compute.internal>
2017-02-27 07:45:57 +0000

    message

---
+++ b.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+b=5
+if [ $b -gt 4 ]
+then
+echo "b is grater"
+else 
+echo "c is grater"
+exit 1
+fi
---
+++ c.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+c=3
+if [ $c -gt 2 ]
+then
+echo "c is grater"
+else 
+echo "a is grater"
+exit 1
+fi
